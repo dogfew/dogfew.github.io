1584263509af32af11bec626ea8962b96418ab58
Vladimir Perepelkin <79280805+dogfew@users.noreply.github.com>
2021-11-11 13:10:07 +0300

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,26 @@
-let myButton = document.querySelector('button');
+let helloButton = document.getElementById("helloButton");
+let byeButton= document.getElementById("byeButton");
+let askButton= document.getElementById("askButton");
+let gayButton= document.getElementById("gayButton");
 
-myButton.onclick = function() {
+helloButton.onclick = function() {
     let name = prompt('Tell me your name!');
-    document.getElementById("script").innerHTML = (`${name}, welcome to the page!`);
+    document.getElementById("script").innerHTML +=
+        "<br>" + (`${name}, you are welcome!`);
+}
+
+byeButton.onclick = function() {
+    let name = prompt('You dare!');
+    document.getElementById("script").innerHTML +=
+        "<br>" + (`${name}, get out of here!`);
+}
+
+askButton.onclick = function() {
+    document.getElementById("script").innerHTML +=
+        "<br>" + "This was created by Vladimir Perepelkin";
+}
+
+gayButton.onclick = function() {
+    document.getElementById("script").innerHTML +=
+        "<br>" + "Yes. I hope, you like it.";
 }
